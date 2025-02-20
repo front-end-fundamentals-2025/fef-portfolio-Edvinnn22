a95a3192077eb40d2dbee333f0b99f87fba90251
Edvinnn22 <edvin.hultqvist@icloud.com>
2025-02-20 16:24:36 +0100

    updated javascript

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Navigation links hover effect
-    const navLinks = document.querySelectorAll(".menu a");
-    navLinks.forEach((link) => {
-      link.addEventListener("mouseover", function () {
-        this.style.color = "red";
-      });
-      link.addEventListener("mouseout", function () {
-        this.style.color = "";
-      });
-    });
-  
-    // Click event on the image
-    const projectImage = document.querySelector(".image img");
-    if (projectImage) {
-      projectImage.addEventListener("click", function () {
-        alert("You clicked the project image!");
-      });
-    }
+// Navigation links hover effect
+const navLinks = document.querySelectorAll(".menu a");
+navLinks.forEach((link) => {
+  link.addEventListener("mouseover", function () {
+    this.style.color = "red";
   });
-  
+  link.addEventListener("mouseout", function () {
+    this.style.color = "";
+  });
+});
+
+// Click event on the image
+const projectImage = document.querySelector(".image img");
+if (projectImage) {
+  projectImage.addEventListener("click", function () {
+    alert("You clicked the project image!");
+  });
+}
